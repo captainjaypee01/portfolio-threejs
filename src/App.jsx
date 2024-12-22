905dc9d26067eb7279dcc5c74e7e462d752cd21c
John Paul Dala <jaypee@lingjack.com>
2024-12-22 22:03:04 +0800

    Add Contact Section with emailjs package

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
 import Clients from "./sections/Clients.jsx";
+import Contact from "./sections/Contact.jsx";
 
 const App = () => {
     return (
@@ -13,6 +13,7 @@ const App = () => {
             <About/>
             <Projects/>
             <Clients/>
+            <Contact/>
         </main>
     )
 }
